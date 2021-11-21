54f0831d36555b6ae8ad5bca26909f9cf2f98125
Masood Shaterabadi <masoodsh77@gmail.com>
2021-11-21 18:12:06 +0330

    add main Card

---
+++ src/Components/Main/Card/Card.tsx
@@ -0,0 +1,35 @@
+import classes from "./Card.module.css";
+import React from "react";
+import { FaTwitter } from "react-icons/fa";
+import { Button } from "@mui/material";
+
+const Card = () => {
+  return (
+    <div className={classes.Card}>
+      <div className={classes.CardBody}>
+        <div className={classes.RightSide}>
+          <div>
+            <FaTwitter className={classes.CardIcon} />
+            <h4>توییتر</h4>
+          </div>
+          <div>
+            <span> آی دی (ID) :</span>
+            <div className={classes.cardId}>@masood</div>
+          </div>
+          <div>
+            <span> لینک :</span>
+            <div className={classes.cardLink}>
+              http://twitter.com/masoodsh73
+            </div>
+          </div>
+        </div>
+        <div>
+          <Button color="warning">ویرایش</Button>
+          <Button color="error">حذف</Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- src/Components/Main/Main.tsx
+++ src/Components/Main/Main.tsx
@@ -1,17 +1,28 @@
+import classes from "./Main.module.css";
 import React from "react";
+import { Button } from "@mui/material";
+import {FaPlus} from 'react-icons/fa'
+import Card from "./Card/Card";
 
 const Main = () => {
   return (
     <div>
       <div>
-        <h3>حساب کاربری</h3>
+        <h2>حساب کاربری</h2>
       </div>
-      <div>
+      <div className={classes.Way}>
         <span>خانه</span>
         <span>.</span>
         <span>کاربر</span>
         <span>.</span>
-        <span>تنظیمات کاربری</span>
+        <span className={classes.WayNotActive}>تنظیمات کاربری</span>
+      </div>
+      <div className={classes.BoxForm}>
+        <div className={classes.mainBox}>
+          <h5>مسیر های ارتباطی</h5>
+          <Button color="warning"> <FaPlus className={classes.PlusIcon}/> افزودن مسیر ارتباطی </Button>
+          <Card/>
+        </div>
       </div>
     </div>
   );
